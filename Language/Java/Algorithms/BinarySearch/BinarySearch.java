08e5ae9fe601cc0679608c337a5f6f264fcc0728
RyanCodesStuff <ryanmyatthu@gmail.com>
2024-09-16 19:11:46 +0630

    Added Binary Search

---
+++ Language/Java/Algorithms/BinarySearch/BinarySearch.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+ package org.JavaAlgorithms.BinarySearch;
+/**
+ *
+ * @author ryanw
+ */
+public class BinarySearch {
+
+    
+}
