a907224c3f338995b7b5166c5cd84486ff7da895
RyanCodesStuff <ryanmyatthu@gmail.com>
2024-09-16 19:59:50 +0630

    Fixed some typos

--- Language/Java/Algorithms/SearchingAlgorithms/BinarySearch/BinarySearch.java
+++ Language/Java/Algorithms/SearchingAlgorithms/BinarySearch/BinarySearch.java
@@ -8,7 +8,7 @@
  * @author ryanw
  */
 public class BinarySearch {
-    public static int search(int[] arr, int target){
+    public static int binarySearch(int[] arr, int target){
         int left = 0;
         int right = arr.length - 1;
         
@@ -17,17 +17,17 @@ public class BinarySearch {
             if(arr[mid] == target){
                 return mid;
             } 
-            if(arr[mid] < right){
+            if(arr[mid] < target){
+                left = mid + 1;            }
+            if(arr[mid] > target){
                 right = mid - 1;
-            }
-            if(arr[mid] > right){
-                left = mid + 1;
             }
         }
         return -1;
     }
     public static void main(String[] args) {
         
+        System.out.println(binarySearch(new int[] {1,2,4,5,6,10}, 2));
     }
 
     
