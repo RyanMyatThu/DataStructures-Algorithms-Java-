bd046ff3eb206aaed0d9f512420bb60f6dd67224
RyanCodesStuff <ryanmyatthu@gmail.com>
2024-12-23 15:04:39 +0630

    BSTalmostDone

--- Language/Java/DataStructures/BinarySearchTrees/BST/BST.java
+++ Language/Java/DataStructures/BinarySearchTrees/BST/BST.java
@@ -1,5 +1,7 @@
 package Language.Java.DataStructures.BinarySearchTrees.BST;
 
+import Language.Java.DataStructures.BinarySearchTrees.Entry;
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class BST<K,V> {
@@ -45,5 +47,71 @@ public class BST<K,V> {
         else return search(node.right, key);
     }
     
+    public V insert(K key, V value){
+        root = put(root, key, value);
+        return value;
+    }
+
+    private Node<K,V> put(Node<K,V> node, K key, V value){
+        if(node == null){
+            size++;
+            return new Node<>(key, value);
+        }
+        int comp = comparator.compare(node.key, key);
+        if(comp < 0){
+            node.left = put(node.left, key, value);
+        } else if (comp > 0){
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    public V remove(K key){
+        Node<K,V> nodeToRemove = search(root, key);
+        if(nodeToRemove == null){
+            return null;
+        }
+        root = remove(root, key);
+        size--;
+        return nodeToRemove.value;
+    }
+    
+    private Node<K,V> remove(Node<K,V> node, K key){
+        if(node == null){
+            return null;
+        }
+        int comp = comparator.compare(node.key, key);
+        if(comp < 0){
+            node.left = remove(node.left, key);
+        } else if (comp > 0){
+            node.right = remove(node.right, key);
+        } else {
+            if(node.left == null) return node.right;
+            if(node.right == null) return node.left;
+ 
+            node.key = findMin(node.right).key;
+            node.value = findMin(node.right).value;
+            node.right = remove(node.right, node.key);
+        }
+        return node;
+    }
+    public Iterable<Entry<K, V>> entrySet() {
+        ArrayList<Entry<K, V>> entries = new ArrayList<>();
+        inorderTraversal(root, entries);
+        return entries;
+    }
+    private Node<K, V> findMin(Node<K, V> node) {
+        while (node.left != null) node = node.left;
+        return node;
+    }
+    private void inorderTraversal(Node<K, V> node, ArrayList<Entry<K, V>> entries) {
+        if (node != null) {
+            inorderTraversal(node.left, entries);
+            entries.add(new Entry<>(node.key, node.value));
+            inorderTraversal(node.right, entries);
+        }
+    }
     
 }
